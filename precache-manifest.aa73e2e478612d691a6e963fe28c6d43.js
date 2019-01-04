3161644bb9bd5b19e73ec50f78a7159ad1c82748
kpazfagundes <kpazfagundes@gmail.com>
2019-01-04 16:08:31 -0200

    Updates

---
+++ precache-manifest.aa73e2e478612d691a6e963fe28c6d43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "35563e7a793c4b1a976e",
+    "url": "/pycommunities/static/js/runtime~main.35563e7a.js"
+  },
+  {
+    "revision": "3034d529b0b63dbdb4f5",
+    "url": "/pycommunities/static/js/main.3034d529.chunk.js"
+  },
+  {
+    "revision": "ee859e6ffc4eba82da8e",
+    "url": "/pycommunities/static/js/1.ee859e6f.chunk.js"
+  },
+  {
+    "revision": "3034d529b0b63dbdb4f5",
+    "url": "/pycommunities/static/css/main.10f4f030.chunk.css"
+  },
+  {
+    "revision": "efb435d92622333678678aa152bf332d",
+    "url": "/pycommunities/index.html"
+  }
+];
